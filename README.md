697e767461426276d05464ac9f14658077a1ddaf
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-25 19:37:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Words-2-Speech
+From a list of words, and a list of sound files, make a list of words into a sound.
---
+++ main.py
@@ -0,0 +1,26 @@
+# Words2Speech
+# Copyright (c) 2023 Krafter et. al.
+from playsound import playsound
+import yaml
+import sys
+
+# Open words.yml
+with open('words.yml', 'r') as file:
+    words = yaml.load(file, Loader=yaml.FullLoader)
+
+# Play a word
+def play(word):
+    if word in words:
+        print(word)
+        playsound(words[word])
+    else:
+        raise NotImplementedError(f'The word "{word}" was not found in "words.yml"!')
+
+
+# Get input
+sentence = sys.argv
+
+# Play sentence
+print(f'Playing words: {sentence}')
+for word in sentence:
+    play(word)
