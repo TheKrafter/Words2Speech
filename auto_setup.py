3fbc3baf0809a608fbd76dec15eaceba9d9c31b4
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-25 19:56:48 -0500

    fsdafdsf

--- auto_setup.py
+++ auto_setup.py
@@ -7,7 +7,7 @@ del words[0]
 
 words_yml = [f'# Start of automated setup with format {audio_format}\n']
 for word in words:
-    line = f"{word.lower()}: '{audio_format.replace('WORDHERE', word)}'"
+    line = f"{word.lower()}: '{audio_format.replace('WORDHERE', word).replace('Wordhere', word.title())}'"
     words_yml.append(f'{line}\n')
     print(line)
 
